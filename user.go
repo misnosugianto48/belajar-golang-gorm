07d3a66bdd4edded3b622c74b38c3e3129a01ec4
misnosugianto48 <misnosugianto48@gmail.com>
2023-12-24 19:47:10 +0700

    Model

--- user.go
+++ user.go
@@ -3,9 +3,13 @@ package belajargolanggorm
 import "time"
 
 type User struct {
-	ID        string
-	Password  string
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `gorm:"primary_key;column:id"`
+	Password  string    `gorm:"column:password"`
+	Name      string    `gorm:"column:name"`
+	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+}
+
+func (u *User) TableName() string {
+	return "users"
 }
